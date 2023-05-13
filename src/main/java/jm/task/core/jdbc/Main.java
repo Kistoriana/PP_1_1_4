c434fa266974016ac8cd5566f829e0f66ec4f365
Victoria Markova <kistoriana@gmail.com>
2023-05-14 02:25:17 +0300

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,30 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+//        Util.getConnection();
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Михаил", "Булгаков", (byte) 30);
+        System.out.println("User с именем – " + userService.getAllUsers().get(0).getName() + " добавлен в базу данных");
+        userService.saveUser("Федор", "Достоевский", (byte) 19);
+        System.out.println("User с именем – " + userService.getAllUsers().get(1).getName() + " добавлен в базу данных");
+        userService.saveUser("Иосиф", "Бродский", (byte) 24);
+        System.out.println("User с именем – " + userService.getAllUsers().get(2).getName() + " добавлен в базу данных");
+        userService.saveUser("Анна", "Ахматова", (byte) 38);
+        System.out.println("User с именем – " + userService.getAllUsers().get(3).getName() + " добавлен в базу данных");
+
+        System.out.println(userService.getAllUsers().toString());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+    }
+}
