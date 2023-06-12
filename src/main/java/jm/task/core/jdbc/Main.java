0f6d063a64c01a06663e05df0ff51f98337269b1
Victoria Markova <kistoriana@gmail.com>
2023-06-12 22:34:10 +0300

    Исправление ошибок 2.0

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,23 +8,23 @@ import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
 
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
 
-        userDaoJDBC.createUsersTable();
+        userDao.createUsersTable();
 
-        userDaoJDBC.saveUser("Михаил", "Булгаков", (byte) 30);
-        System.out.println("User с именем – " + userDaoJDBC.getAllUsers().get(0).getName() + " добавлен в базу данных");
-        userDaoJDBC.saveUser("Федор", "Достоевский", (byte) 19);
-        System.out.println("User с именем – " + userDaoJDBC.getAllUsers().get(1).getName() + " добавлен в базу данных");
-        userDaoJDBC.saveUser("Иосиф", "Бродский", (byte) 24);
-        System.out.println("User с именем – " + userDaoJDBC.getAllUsers().get(2).getName() + " добавлен в базу данных");
-        userDaoJDBC.saveUser("Анна", "Ахматова", (byte) 38);
-        System.out.println("User с именем – " + userDaoJDBC.getAllUsers().get(3).getName() + " добавлен в базу данных");
+        userDao.saveUser("Михаил", "Булгаков", (byte) 30);
+        System.out.println("User с именем – " + userDao.getAllUsers().get(0).getName() + " добавлен в базу данных");
+        userDao.saveUser("Федор", "Достоевский", (byte) 19);
+        System.out.println("User с именем – " + userDao.getAllUsers().get(1).getName() + " добавлен в базу данных");
+        userDao.saveUser("Иосиф", "Бродский", (byte) 24);
+        System.out.println("User с именем – " + userDao.getAllUsers().get(2).getName() + " добавлен в базу данных");
+        userDao.saveUser("Анна", "Ахматова", (byte) 38);
+        System.out.println("User с именем – " + userDao.getAllUsers().get(3).getName() + " добавлен в базу данных");
 
-        System.out.println(userDaoJDBC.getAllUsers().toString());
-        userDaoJDBC.cleanUsersTable();
-        userDaoJDBC.dropUsersTable();
+        System.out.println(userDao.getAllUsers().toString());
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
